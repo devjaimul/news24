b9459fb3e78bfe714e564ccbb26e155d7b4ee21f
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-09-05 04:20:56 +0600

    updated

--- lib/utlis/colors.dart
+++ lib/utlis/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   //static Color primaryColor=const Color(0xff33907C);
-  static Color primaryColor=Colors.teal;
+  static Color primaryColor=Colors.blue;
   static Color secondaryColor=Colors.white;
 
 }
